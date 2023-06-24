0c1ef548c0a4d40b57e0bfbc7f6bf01db09b6478
Albert Tran <alberttran0810@gmail.com>
2023-06-24 14:28:22 +0700

    Get metadata of azero domain nft when updating nft cron

--- Api/src/cronjobs/actions.ts
+++ Api/src/cronjobs/actions.ts
@@ -522,7 +522,7 @@ export async function check_NFT_queue_all(
                 }
                 // Check and update attributes
                 console.log(`${CONFIG_TYPE_NAME.AZ_PROCESSING_ALL_QUEUE_NFT} - nft_owner: `, nft_owner);
-
+                
                 // Get NFT's Attributes
                 const metaData = {
                     traits: undefined,
@@ -552,6 +552,24 @@ export async function check_NFT_queue_all(
                     nftContractAddress,
                     {bytes: azDomainName}
                 );
+
+                const {data: domainMetadata} = await axios({
+                    url: `https://tzero.id/api/v1/metadata/${azDomainName}.tzero.json`,
+                    method: "get",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "cache-control": "no-cache",
+                        "Access-Control-Allow-Origin": "*",
+                    },
+                });
+                if (domainMetadata) {
+                    if (domainMetadata.metadata) {
+                        metaData.nftName = domainMetadata.metadata.name;
+                        metaData.traits = domainMetadata?.metadata?.attributes?.reduce((p: any, c: any) => {
+                            return {...p, [c.trait_type]: c.value};
+                        }, {});
+                    }
+                }
                 // console.log(`${CONFIG_TYPE_NAME.AZ_PROCESSING_ALL_QUEUE_NFT} - forSaleInformation: `, forSaleInformation);
                 let obj: nfts = new nfts(
                     {
