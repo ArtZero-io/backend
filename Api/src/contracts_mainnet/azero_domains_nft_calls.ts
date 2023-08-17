8239b61d501efb6d0e2291b878ba093bcb7ce189
AlbertTran <53705980+Albert-Tran@users.noreply.github.com>
2023-08-17 15:07:09 +0700

    Add folder contract for mainnet and testnet

---
+++ Api/src/contracts_mainnet/azero_domains_nft_calls.ts
@@ -0,0 +1,180 @@
+import BN from "bn.js";
+import {convertNumberWithoutCommas, isValidAddressPolkadotAddress, readOnlyGasLimit} from "../utils/utils";
+import {ContractPromise} from "@polkadot/api-contract";
+import {ApiPromise} from "@polkadot/api";
+import {azero_domains_nft} from "./azero_domains_nft";
+import {AzeroDomainEventRepository} from "../repositories";
+
+let azero_domain_nft_contract: ContractPromise;
+
+export function setContract(c: ContractPromise) {
+    azero_domain_nft_contract = c;
+}
+
+export const setAzeroDomainsNFTContract = (api: ApiPromise) => {
+    azero_domain_nft_contract = new ContractPromise(
+        api,
+        azero_domains_nft.CONTRACT_ABI,
+        azero_domains_nft.CONTRACT_ADDRESS
+    );
+};
+
+export async function balanceOf(
+    api: ApiPromise,
+    caller_account: string,
+    account: string
+): Promise<number> {
+    try {
+        if (!azero_domain_nft_contract || !caller_account || !account) {
+            return 0;
+        }
+        const address = caller_account;
+        const gasLimit = readOnlyGasLimit(api);
+        const azero_value = 0;
+
+        const { result, output } = await azero_domain_nft_contract.query["psp34::balanceOf"](
+            address,
+            { value: azero_value, gasLimit },
+            account
+        );
+        if (result.isOk && output) {
+            // @ts-ignore
+            const ret = output.toHuman()?.Ok;
+            return parseInt(ret.replaceAll(",", ""));
+        }
+    } catch (e) {
+        console.log(`ERROR: ${e.messages}`);
+    }
+    return 0;
+}
+
+export async function totalSupply(
+    caller_account: string
+): Promise<number | null> {
+    if (!azero_domain_nft_contract || !caller_account) {
+        return 0;
+    }
+    const address = caller_account;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(azero_domain_nft_contract.api)
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await azero_domain_nft_contract.query["psp34::totalSupply"](
+        address,
+        {value: azero_value, gasLimit}
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return parseInt(convertNumberWithoutCommas(output.toHuman()?.Ok));
+    }
+    return 0;
+}
+
+export async function ownerOf(
+    caller_account: string,
+    domainName: string,
+):Promise<string | null> {
+    console.log({
+        caller_account: caller_account,
+        domainName:domainName,
+        azero_domain_nft_contract: azero_domain_nft_contract
+    });
+
+    if (!azero_domain_nft_contract || !caller_account) {
+        return null;
+    }
+    const address = caller_account;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(azero_domain_nft_contract.api)
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await azero_domain_nft_contract.query["psp34::ownerOf"](
+        address,
+        {value: azero_value, gasLimit},
+        {bytes: domainName}
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        console.log(output.toHuman());
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function getLockInfo(
+    caller_account: string,
+    domainName: string,
+):Promise<string | null> {
+    if (!azero_domain_nft_contract || !caller_account) {
+        return null;
+    }
+    const address = caller_account;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(azero_domain_nft_contract.api)
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await azero_domain_nft_contract.query["getLockInfo"](
+        address,
+        {value: azero_value, gasLimit},
+        domainName
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        console.log(output.toHuman());
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function getNftOwner(
+    caller_account: string,
+    domainName: string,
+):Promise<string | null> {
+    if (!azero_domain_nft_contract || !caller_account) {
+        return null;
+    }
+    const address = caller_account;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(azero_domain_nft_contract.api)
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await azero_domain_nft_contract.query["getOwner"](
+        address,
+        {value: azero_value, gasLimit},
+        domainName
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok?.Ok;
+    }
+    return null;
+}
+
+export async function getRegistrationPeriod(
+    callerCccount: string,
+    domainName: string,
+):Promise<any[]> {
+    if (!azero_domain_nft_contract || !callerCccount) {
+        return [];
+    }
+    const address = callerCccount;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(azero_domain_nft_contract.api)
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await azero_domain_nft_contract.query["getRegistrationPeriod"](
+        address,
+        {value: azero_value, gasLimit},
+        domainName
+    );
+    if (result.isOk && output) {
+        const ret = output.toHuman();
+        console.log(ret);
+        // @ts-ignore
+        return ret?.Ok?.Ok;
+    }
+    return [];
+}
+
---
+++ Api/src/contracts_testnet/staking_calls.ts
@@ -0,0 +1,212 @@
+import {ContractPromise} from "@polkadot/api-contract";
+import BN from "bn.js";
+import {getGasLimit, readOnlyGasLimit} from "../utils/utils";
+import {KeyringPair} from "@polkadot/keyring/types";
+
+let staking_contract: ContractPromise;
+
+export function setContract(c: ContractPromise) {
+    staking_contract = c;
+}
+
+export async function getTotalStaked(caller_account: string) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    const address = caller_account;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::getTotalStaked"](
+        address,
+        {
+            value: azero_value,
+            gasLimit
+        }
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return new BN(output.toHuman()?.Ok, 10, "le").toNumber();
+    }
+    return null;
+}
+
+export async function isClaimed(caller_account: any, account: string) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    // @ts-ignore
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::isClaimed"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        },
+        account
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function getIsLocked(caller_account: any) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::getIsLocked"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        }
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function getRewardStarted(caller_account: any) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::getRewardStarted"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        }
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function getTotalCountOfStakeholders(caller_account: any): Promise<number> {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return 0;
+    }
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::getStakedAccountsLastIndex"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        }
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return new BN(output.toHuman()?.Ok, 10, "le").toNumber();
+        // return new BN(output, 10, "le").toNumber();
+    }
+    return 0;
+}
+
+export async function getStakedAccountsAccountByIndex(caller_account: any, index: number) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const {result, output} = await staking_contract.query["artZeroStakingTrait::getStakedAccountsAccountByIndex"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        },
+        index
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return null;
+}
+
+export async function setClaimedStatus(keypair: KeyringPair, caller:string, account: string) {
+    const gasLimitResult = await getGasLimit(
+        staking_contract.api,
+        caller,
+        "setClaimedStatus",
+        staking_contract,
+        {},
+        [account]
+    );
+    if (!gasLimitResult.ok) {
+        // @ts-ignore
+        console.log('gasLimitResult.error', gasLimitResult?.error);
+        return;
+    }
+    const { value: gasLimit } = gasLimitResult;
+    const value = 0;
+    // @ts-ignore
+    await staking_contract.tx.setClaimedStatus({ gasLimit, value },account)
+        .signAndSend(keypair, result => {
+            if (result.status.isInBlock) {
+                console.log('in a block');
+            } else if (result.status.isFinalized) {
+                console.log('finalized');
+            }
+        })
+        .catch((e) => console.log("e", e));
+}
+
+export async function isAdmin(caller_account: any, account: string) {
+    if (!staking_contract || !caller_account) {
+        console.log("invalid inputs");
+        return null;
+    }
+    const address = caller_account?.address;
+    // @ts-ignore
+    const gasLimit = readOnlyGasLimit(staking_contract.api);
+    const azero_value = 0;
+    // @ts-ignore
+    const { result, output } = await staking_contract.query["accessControl::hasRole"](
+        address,
+        {
+            value: azero_value,
+            gasLimit,
+        },
+        '3739740293',
+        account
+    );
+    if (result.isOk && output) {
+        // @ts-ignore
+        return output.toHuman()?.Ok;
+    }
+    return false;
+}
