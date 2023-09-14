5aa4d784400860ca7e2649a1a9f25ac78ea5639a
huypham <huyp26102000@gmail.com>
2023-09-14 16:25:23 +0700

    Revert "add log scan nft"
    This reverts commit 6a227c45b0e4ea9afe3814e9a5baa57f04759ca9.

--- Api/src/utils/utils.ts
+++ Api/src/utils/utils.ts
@@ -180,7 +180,7 @@ export const APICall = {
                 `/getJSON?input=${param.tokenUri}${param.tokenID}.json`,
                 {}
             );
-            console.log("getMetadataOffChain ret", ret);
+            // console.log("getMetadataOffChain ret", ret);
             return ret;
         } catch (e) {
             return null;
